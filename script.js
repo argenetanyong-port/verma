326b512298427ffe1d598810acf17d71a70dd4f7
argene <argene_tanyong@yahoo.com>
2021-07-27 00:53:43 +0800

    wave animation integration

---
+++ script.js
@@ -0,0 +1,7 @@
+var animation = lottie.loadAnimation({
+  container: document.getElementById("hero-banner-wave"),
+  renderer: "svg",
+  loop: true,
+  autoplay: true,
+  path: "data-color-1.json",
+});
